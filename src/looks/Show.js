5149cd6a2c20cd2b26789804020897e05a2318a3
vidyaargade <160412138+vidyaargade@users.noreply.github.com>
2024-10-07 17:31:45 +0530

    Add files via upload

---
+++ src/looks/Show.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { connect } from "react-redux";
+import Paper from "@material-ui/core/Paper";
+
+const Show = ({ character, comp_id }) => {
+  const handleDisplay = () => {
+    const el = document.getElementById(character.active);
+    el.style.display = "inline-block";
+  };
+
+  return (
+    <Paper elevation={3}>
+      <div
+        id={comp_id}
+        className="rounded text-center bg-purple-700 text-white px-2 py-1 my-2 text-sm cursor-pointer mx-auto"
+        onClick={() => handleDisplay()}
+      >
+        Show
+      </div>
+    </Paper>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    character: state.character,
+  };
+};
+
+export default connect(mapStateToProps)(Show);
